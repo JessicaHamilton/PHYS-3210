aa5521e50a3ffc0af98c6d1f99fc35a54403e80f
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-08-29 09:07:20 -0400

    update of ex.4

--- Week 02/exercise04.py
+++ Week 02/exercise04.py
@@ -15,7 +15,7 @@ time = np.linspace(0,10,101)
 A = 3
 B = 0
 alpha = 12
-beta = 30
+beta = 20
 
 viral_load = A*np.exp(-alpha*time)+B*np.exp(-beta*time)
 
@@ -24,9 +24,14 @@ plt.title("Viral Load versus Time")
 
 
 #Now for dataset
+#load data and assign variables
 HIV_data = np.loadtxt(r"\Users\hamil\Documents\Github\PHYS-3210\Week 01\data\HIVseries.csv")
 print(HIV_data)
-
+data_time = HIV_data[0]
+print(data_time)
+data_viralload = HIV_data[1]
+print(viralload)
+plt.plot(data_time, data_viralload)
 
 
 

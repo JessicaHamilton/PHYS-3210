9e5d04514d9ca80144d2e4ad913bc6928f4d8213
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-08-30 08:41:24 -0400

    EX04 complete.

--- Week 02/exercise04.py
+++ Week 02/exercise04.py
@@ -55,30 +55,53 @@ def fitting(A,B,alpha,beta,time):
     plt.title("Concentration of Viral Load Versus Time")
     plt.xlabel("Time")
     plt.ylabel("Concentration")
+    plt.legend()
     return first,second
 
 
-#Testing Out the function by varying alpha and A while beta and B are fixed
-#plot all and save to a pdf
-    
-test1 = fitting(240000,900,1,1000,time)
-#test2 = fitting(2000,900,5,1000,time)
-#test3 = fitting(10000,900,4,1000,time)
-#test4 = fitting(50000,900,3,1000,time)
-#test5 = fitting(100000,900,2,1000,time)
 
-plt.scatter(test1)
-#plt.scatter(test2)
-#plt.scatter(test3)
-#plt.scatter(test4)
-#plt.scatter(test5)
+
+def fitting(A,B,alpha,beta,time):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    #define equation
+    v_load = A*np.exp(-alpha*time)+B*np.exp(-beta*time)
+    return v_load
+
+#Testing out the function by varying alpha and A while beta and B are fixed
+#plot all and save to a pdf
+
+HIV_data = np.load(r"\Users\hamil\Documents\Github\PHYS-3210\Week 01\data\HIVseries.npz")
+data_time = HIV_data['time_in_days']
+data_viralload = HIV_data['viral_load']
+second = plt.scatter(data_time, data_viralload, color = 'navy', label= 'Actual')
 plt.title("Concentration of Viral Load Versus Time")
 plt.xlabel("Time")
 plt.ylabel("Concentration")
+plt.legend()
+
+time = np.linspace(0,10,101)
+test1 = fitting(240000,900,1,1000,time)
+test2 = fitting(2000,900,5,1000,time)
+test3 = fitting(10000,900,4,1000,time)
+test4 = fitting(50000,900,3,1000,time)
+test5 = fitting(100000,900,2,1000,time)
+test6 = fitting(240000,900,0.80,1000,time)
+
+plt.scatter(time,test2,s=2, Label='Test1')
+plt.scatter(time,test3,s=2, label='Test2')
+plt.scatter(time,test4,s=2, label='Test3')
+plt.scatter(time,test5,s=2, label='Test4')
+plt.scatter(time,test1,s=2, label='Test5')
+plt.scatter(time,test6, s=2, label= 'Test6')
+plt.title("Concentration of Viral Load Versus Time")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+plt.legend()
+plt.savefig("TestingViralLoad.pdf")
 
 
 
-#Cheak to see how we are to do this...load outside or inside?? time or no time
 
 
 

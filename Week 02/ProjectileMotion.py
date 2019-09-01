46e4885337fcc3a647410cd8a711116f11964cf3
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-01 11:36:44 -0400

    Update ProjectileMotion.py

--- Week 02/ProjectileMotion.py
+++ Week 02/ProjectileMotion.py
@@ -10,34 +10,38 @@ import matplotlib.pyplot as plt
 
 def projectile(mass,initial_pos,initial_v, launch_angle):
     force_g = 9.8
-    speed_c = 3.0e08
-    time = np.linspace(0,50,0.1)
+    time = np.arange(0,100,0.1)
     final_x_array =[]
     final_y_array =[]
     velocity_x_array =[]
     velocity_y_array =[]
-    change_time = time[1]-time[0]
+    total_time = 0
+    change_time = 0.1
     for each_time in time:
-        #calculate output variables
+        #calculate output variables and append to array
         final_x = (initial_pos*np.cos(launch_angle))+((initial_v*np.cos(launch_angle))*change_time)
-        final_y = (initial_pos*np.sin(launch_angle))+((initial_v*np.sin(launch_angle))*change_time)
-        if final_y < 0:
-            continue
-        velocity_x = initial_v*np.cos(launch_angle)
-        velocity_y = (initial_v*np.sin(launch_angle))+((-(force_g)/mass)*change_time)
-        #re-define input variables
-        initial_pos = np.tan(final_y/final_x)
-        initial_v = np.tan(velocity_y/velocity_x)
-        #append to arrays
         final_x_array.append(final_x)
+        final_y = (initial_pos*np.sin(launch_angle))+((initial_v*np.sin(launch_angle))*change_time)
         final_y_array.append(final_y)
+        velocity_x = initial_v*np.cos(launch_angle)
         velocity_x_array.append(velocity_x)
+        velocity_y = (initial_v*np.sin(launch_angle))+((-(force_g)/mass)*change_time)
         velocity_y_array.append(velocity_y)
-    total_time = each_time - time[0]
+        
+        #re-define input variables
+        initial_pos = np.sqrt(final_y**2 + final_x**2)
+        initial_v = np.sqrt(velocity_y**2 + velocity_x**2)
+        #print(initial_v)
+        if final_y <= 0:
+            break
+    total_time = each_time - 0
+    #final_x_array = np.unique(final_x_array)
+    final_y_array = np.unique(final_y_array)
     return final_x_array,final_y_array,velocity_x_array, velocity_y_array, total_time
 
-test1 = projectile(5,5,10,15)
-x_values = test1['final_x_array']
-y_values = test1['final_y_array']
-plt.scatter(x_values,y_values)
+test1 = projectile(5,0,10,np.radians(30))
+#print(test1[0])
+x_values = test1[0]
+y_values = test1[1]
+plt.scatter(x_values,y_values, s=2)
     

3067fea80231b474df04dec1c2f4578919c303d7
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-05 15:58:25 -0400

    Update exercise05.py

--- Week 03/exercise05.py
+++ Week 03/exercise05.py
@@ -19,12 +19,12 @@ def up_harmonic(value_n):
         numm1 = 1/each_value
         H_up = H_up + numm1
         summ_array1.append(H_up)
-    return H_up, summ_array1, x_array1
-test = up_harmonic(20)
-up_values = test[1]
-x_values = test[2]
-print("Sum up value is:", test[0]) 
-print("The Up values are:", up_values)
+    return H_up, summ_array1
+#test = up_harmonic(20)
+#up_sum = test[0]
+#up_values = test[1]
+#print("Sum up value is:", test[0]) 
+#print("The Up values are:", up_values)
 
 
 
@@ -38,17 +38,32 @@ def down_harmonic(value_n):
         numm2 = 1/each_value
         H_down = H_down + numm2
         summ_array2.append(H_down)
-    return H_down, summ_array2, x_array2
+    return H_down, summ_array2
 
-test1 = down_harmonic(20)
-down_values = test1[1]
-print("Sum down value is:", test1[0])
-print("The down values are:", down_values)
+#test1 = down_harmonic(20)
+#down_sum = test1[0]
+#down_values = test1[1]
+#print("Sum down value is:", test1[0])
+#print("The down values are:", down_values)
 
 
-x_values = np.arange(1,20)
-#sub = np.subtract(up_values, down_values)
-#abs_add = np.sum(np.abs(up_values), np.abs(down_values))
-#fraction = sub / abs_add
-#plt.plot(x_values,fraction)
+
+
+fraction_array = []
+x_values = np.arange(1,50)
+for new_value in x_values:
+    test1 = up_harmonic(new_value)
+    test2 = down_harmonic(new_value)
+    up_sum = test1[0]
+    down_sum = test2[0]
+    up_array = test1[1]
+    down_array = test2[1]
+    print("The up sum is:", up_sum)
+    print("The down sum is:", down_sum)
+    sub = up_sum - down_sum
+    abs_add = np.abs(up_sum) + np.abs(down_sum)
+    fraction = sub / abs_add
+    fraction_array.append(fraction)
+    
+plt.plot(x_values,fraction_array)
 

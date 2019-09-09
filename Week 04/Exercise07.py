d0f0bee1baa8b92c4a5783e614609540d6ef6f1d
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-09 11:20:29 -0400

    Create Exercise07.py

---
+++ Week 04/Exercise07.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 10:05:15 2019
+
+@author: hamil
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from Users\hamil\Documents\Github\PHYS-3210\Week 04\ import rand.py 
+from rand.py import powerResidue
+
+
+first_seq = powerResidue(13)
+print(first_seq)

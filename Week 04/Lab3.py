97f8d62ab6ccfd3aa8445058cc1960fd7396b3f6
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-11 13:13:17 -0400

    Update Lab3.py

--- Week 04/Lab3.py
+++ Week 04/Lab3.py
@@ -9,30 +9,34 @@ import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Define the x,y,N initilize the coordinate array
 x = 0
 y = 0
 x_y_array = [[0,0]]
-N = 15
-
+N = 30
+#loop for determining the new coordinates
 for each_value in range(N):
+    #define the random int for x and y = to -1,0,1
     x_next = rand.randint(-1,1)
     y_next = rand.randint(-1,1)
-    if np.abs(x_next) == np.abs(y_next):
-        continue
-    x = x + x_next
-    y = y + y_next
-    coord = [x,y]
-    if coord in x_y_array == True:
-        continue
-    else:
-        x_y_array.append(coord)
-    if np.abs(x_next) >= 5 or np.abs(y_next) >=5:
-        break
+    
+    # the two do not match:
+    if abs(x_next) != abs(y_next):
+        x = x + x_next
+        y = y + y_next
+        if abs(x) <= 5 or abs(y) <=5:
+            coord = [x,y]
+            if coord in x_y_array:
+                continue
+            else:
+                x_y_array.append(coord)
 
-
-#x_y_array = np.array(x_y_array)
-print(x_y_array)
-plt.plot(x_y_array[0],x_y_array[1])
+print("This is the array of coordinates:", x_y_array)
+x_y_array = np.array(x_y_array)
+x_values = x_y_array[:,0]
+y_values = x_y_array[:,1]
+plt.plot(x_values, y_values)
+plt.title("Random Walker Self-Avoiding Path")
 plt.show()
     
         

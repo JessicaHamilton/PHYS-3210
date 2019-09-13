40894e02277c11817048f280256ebf707a710160
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-13 11:15:46 -0400

    continue updates on lab 3

--- Week 04/Lab3.py
+++ Week 04/Lab3.py
@@ -14,8 +14,9 @@ x = 0
 y = 0
 x_y_array = [[0,0]]
 N = 800
-polar_array = [[]]
-nonpolar_array = [[]]
+polar_array = []
+nonpolar_array = []
+count = 0
 #loop for determining the new coordinates
 for each_value in range(N):
     #define the random int for x and y = to -1,0,1
@@ -23,14 +24,20 @@ for each_value in range(N):
     y_next = rand.randint(-1,1)
     
     # the two do not match:
+    #check if the values match to not make diagonals
     if abs(x_next) != abs(y_next):
+        #create temp coordinate
         x_maybe = x + x_next
         y_maybe = y + y_next
+        # check if greater than 5
         if abs(x_maybe) <= 5 or abs(y_maybe) <=5:
+            #make coordinate
             coord = [x_maybe,y_maybe]
+            #check if coordinate exists within array
             if coord in x_y_array:
                 continue
             else:
+                #update x and y
                 x = x_maybe
                 y = y_maybe
                 x_y_array.append(coord)
@@ -39,24 +46,33 @@ for each_value in range(N):
                     polar_array.append(coord)
                 else: 
                     nonpolar_array.append(coord)
+                coord_a = [x_maybe + 1, y_maybe]
+                coord_b = [x_maybe - 1, y_maybe]
+                coord_c = [x_maybe, y_maybe + 1]
+                coord_d = [x_maybe, y_maybe - 1]
+                new_array = [coord_a, coord_b, coord_c, coord_c]
+                if new_array in nonpolar_array:
+                    count = count + 1
+                else:
+                    count = count
 
 print("This is the array of coordinates:", x_y_array)
 print("Polar Monomers:", polar_array)
 print("Non-polar array:", nonpolar_array)
+print("Length of chain:", (len(x_y_array) -1))
 x_y_array = np.array(x_y_array)
 polar_array = np.array(polar_array)
 nonpolar_array = np.array(nonpolar_array)
 x_values = x_y_array[:,0]
 y_values = x_y_array[:,1]
-polar_x = polar_array[:,1]
+polar_x = polar_array[:,0]
 polar_y = polar_array[:,1]
 non_polar_x = nonpolar_array[:,0]
 non_polar_y = nonpolar_array[:,1]
-plt.plot(x_values, y_values, color='purple')
-plt.scatter(polar_x, polar_y, s=3, color = 'pink')
-plt.scatter(non_polar_x, non_polar_y, s=3, color= 'gray')
+plt.plot(x_values, y_values, color='gray')
+plt.scatter(polar_x, polar_y, color = 'pink')
+plt.scatter(non_polar_x, non_polar_y, color= 'purple')
 plt.title("Random Walker Self-Avoiding Path")
-plt.scatter()
 plt.show()
     
         

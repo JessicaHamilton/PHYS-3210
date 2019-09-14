8c27b1f31f996656d591c395c765055ef95c781f
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-13 20:46:57 -0400

    Update Lab3.py

--- Week 04/Lab3.py
+++ Week 04/Lab3.py
@@ -13,53 +13,63 @@ import matplotlib.pyplot as plt
 x = 0
 y = 0
 x_y_array = [[0,0]]
-N = 800
+N = 100
 polar_array = []
 nonpolar_array = []
+test_array = []
 count = 0
+eps = 1
 #loop for determining the new coordinates
 for each_value in range(N):
     #define the random int for x and y = to -1,0,1
     x_next = rand.randint(-1,1)
     y_next = rand.randint(-1,1)
     
-    # the two do not match:
-    #check if the values match to not make diagonals
+    #check if the values match to not make diagonals and create temp values and coord
     if abs(x_next) != abs(y_next):
-        #create temp coordinate
         x_maybe = x + x_next
         y_maybe = y + y_next
-        # check if greater than 5
-        if abs(x_maybe) <= 5 or abs(y_maybe) <=5:
-            #make coordinate
-            coord = [x_maybe,y_maybe]
-            #check if coordinate exists within array
-            if coord in x_y_array:
-                continue
-            else:
-                #update x and y
-                x = x_maybe
-                y = y_maybe
-                x_y_array.append(coord)
-                random_value = rand.random()
-                if random_value >= 0.68:
-                    polar_array.append(coord)
-                else: 
-                    nonpolar_array.append(coord)
-                coord_a = [x_maybe + 1, y_maybe]
-                coord_b = [x_maybe - 1, y_maybe]
-                coord_c = [x_maybe, y_maybe + 1]
-                coord_d = [x_maybe, y_maybe - 1]
-                new_array = [coord_a, coord_b, coord_c, coord_c]
-                if new_array in nonpolar_array:
-                    count = count + 1
+        coord = [x_maybe,y_maybe]
+        
+        #check if temp coordinate exists within array
+        if coord in x_y_array:
+            continue
+        else:
+            #create random value and compare to determine if polar or nonpolar
+            random_value = rand.random()
+            if random_value >= 0.68:
+                polar_array.append(coord)
+            else: 
+                nonpolar_array.append(coord)
+                
+            #calculate neighbors to determine if connected and non-polar for count
+            coord_a = [x_maybe + 1, y_maybe]
+            coord_b = [x_maybe - 1, y_maybe]
+            coord_c = [x_maybe, y_maybe + 1]
+            coord_d = [x_maybe, y_maybe - 1]
+            test_array = [coord_a, coord_b, coord_c, coord_d]
+            for i in test_array:
+                if i != x_y_array[-1]:
+                    if i in nonpolar_array:
+                        count = count + 1
+                    else:
+                        continue
                 else:
-                    count = count
-
-print("This is the array of coordinates:", x_y_array)
+                    continue
+            #append new coordinate to final array, update x and y, calcualte energy
+            x = x_maybe
+            y = y_maybe
+            x_y_array.append(coord)
+            energy = -eps*count
+                
+                
+                
+                
+print("Full coordinate Array:", x_y_array)
 print("Polar Monomers:", polar_array)
-print("Non-polar array:", nonpolar_array)
+print("Non-polar Monomers:", nonpolar_array)
 print("Length of chain:", (len(x_y_array) -1))
+print("Energy of chain:", energy)
 x_y_array = np.array(x_y_array)
 polar_array = np.array(polar_array)
 nonpolar_array = np.array(nonpolar_array)
@@ -69,10 +79,11 @@ polar_x = polar_array[:,0]
 polar_y = polar_array[:,1]
 non_polar_x = nonpolar_array[:,0]
 non_polar_y = nonpolar_array[:,1]
-plt.plot(x_values, y_values, color='gray')
-plt.scatter(polar_x, polar_y, color = 'pink')
-plt.scatter(non_polar_x, non_polar_y, color= 'purple')
+plt.plot(x_values, y_values, color='gray', label = 'Full Chain')
+plt.scatter(polar_x, polar_y, color = 'pink', label = 'Polar')
+plt.scatter(non_polar_x, non_polar_y, color= 'purple', label= 'Non-polar')
 plt.title("Random Walker Self-Avoiding Path")
+plt.legend()
 plt.show()
     
         

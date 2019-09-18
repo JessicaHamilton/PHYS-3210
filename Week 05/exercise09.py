3f64c79248e1ef68ecb4906f278a71a2a280bdad
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-18 10:54:19 -0400

    Update exercise09.py

--- Week 05/exercise09.py
+++ Week 05/exercise09.py
@@ -8,8 +8,23 @@ Created on Wed Sep 18 10:09:32 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-def integral_funct(function, h, left, initial, final):
-    #determine the values for the function
-    x_values = np.arange(0,20,1)
-    y_values = x_values**2
-    #determine the rectangle
+x_values = np.arange(0,20,1)
+y_values = x_values**2
+
+def integral_funct(h, initial, final):
+    #determine the rectangle and the area using left side of rectangle
+    value = y_values[0]
+    area_array = []
+    bound_array =np.arange(initial, final,1)
+    for each_value in bound_array:
+        area = 0.5*h*((value)**2)
+        area_array.append(area)
+        value = value + h
+    total_sum = np.sum(area_array)
+    return area_array, total_sum
+    
+test1 = integral_funct(0.5,0,10)
+test1_area = test1[0]
+print("This is the individual area array:",test1_area)
+test1_sum = test1[1]  
+print("This is the total area:",test1_sum)  

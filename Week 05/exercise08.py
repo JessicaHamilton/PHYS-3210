9d974755f95f825637223f13e17791c071587083
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-18 22:52:08 -0400

    Update exercise08.py

--- Week 05/exercise08.py
+++ Week 05/exercise08.py
@@ -39,26 +39,56 @@ value1,value2 = deriv_function(np.sin, x_array, 1)
 value3 = np.gradient(np.sin(x_array))
 
 #Compare with the numpy function for the gradient
-plt.plot(x_array, value33, color = "purple", label = "Python's Gradient Function")
+plt.plot(x_array, value3, color = "purple", label = "Python's Gradient Function")
 plt.plot(x_array, value1, color = 'blue', label = "Forward Difference")
 plt.plot(x_array, sinx_array, color= 'green', label= "Python's Sin Function")
-plt.plot(x_array, cosx_array, color = 'red', label= "Python's Cos Function")
+plt.plot(x_array, cosx_array, linestyle= 'dashed', color = 'gray', label= "Python's Cos Function")
 plt.legend()
-plt.title("Comparing The Three functions")
+plt.title("Comparing Three functions for First Deratives")
 plt.show()
 
 #Determine the relative error for each function
 forward_error = []
 gradient_error = []
 
-top1 = np.subtract(value1, cosx_array)
-top2 = np.subtract(value3, cosx_array)
-forward_error = abs(top1)/cosx_array
-gradient_error = abs(top2)/cosx_array
-
-plt.plot(forward_error, cosx_array)
-plt.plot(gradient_error, cosx_array)
-plt.title("Relative Error versus Exact value")
+absolute1 = np.subtract(value1, cosx_array)
+absolute2 = np.subtract(value3, cosx_array)
+condition = absolute2 <= 10**-13
+condition |= absolute1 <=10**-13
+absolute2[condition]= 0
+absolute1[condition] = 0
 
 
+forward_error = np.divide(absolute1,cosx_array)
+gradient_error = np.divide(absolute2, cosx_array)
+
+
+plt.errorbar(x_array, value3, yerr=gradient_error)
+plt.errorbar(x_array, value1, yerr=forward_error)
+plt.title("Relative Error for First Derative")
+plt.show()
+
+
+#Now compute the second deratives using both ways and plot to compare
+value5, value6 = deriv_function(np.cos, x_array,1)
+value7 = np.gradient(np.cos(x_array))
+negsinx_array = np.negative(sinx_array)
+
+#now comute the relative error for this group
+absolute3 = np.subtract(value5,negsinx_array)
+absolute4 = np.subtract(value7, negsinx_array)
+forward_err = np.divide(value5, negsinx_array)
+gradient_err = np.divide(value7,negsinx_array)
+
+plt.plot(x_array, value7, color = "purple", label = "Python's Gradient Function")
+plt.plot(x_array, value5, color = 'blue', label = "Forward Difference")
+plt.plot(x_array, negsinx_array, color= 'green', label= "Python's Neg. Sin Function")
+plt.plot(x_array, cosx_array, linestyle = 'dashed', color = 'gray', label= "Python's Cos Function")
+plt.legend()
+plt.title("Comparing Three functions for Second Deratives")
+plt.show()
+
+plt.errorbar(x_array, value5, yerr = forward_err)
+plt.errorbar(x_array, value7, yerr= gradient_err)
+plt.title("Relative Error for Second Derative")
 

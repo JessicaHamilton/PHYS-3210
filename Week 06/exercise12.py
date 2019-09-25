c74b429666eaea5464802d90efabecab1b3e04ae
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-25 10:57:15 -0400

    Create exercise12.py

---
+++ Week 06/exercise12.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 10:11:39 2019
+
+@author: hamil
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as rand
+from numpy import histogram as hist
+
+
+
+#define a function that will take previous mean value function to N-D
+def meanvalue_Int(N, initial,final):
+    #for loop for N-D
+    sum_array = []
+    mean_array = []
+    for n in range(N):
+        #Generate the sampling values for x  and calculate y values
+        #for n in range(N):
+        x = rand.uniform(initial,final)
+        sum_x = np.sum(x)
+        y = x**2
+        
+        
+        
+        #calculate mean value and append to array
+        mean_value = (final - initial)
+        mean_array.append(mean_value)
+        
+    total_mean = np.prod(mean_array)
+    total_sum = np.sum(sum_array)
+    result = (total_mean*total_sum)/N
+    return result, x, y
+    
+    
+
+a = meanvalue_Int(1000,0,10,10)
+b = meanvalue_Int(1000,0,10,10)
+c = meanvalue_Int(1000,0,10,10)
+d = meanvalue_Int(1000,0,10,10)
+
+print(a,b,c,d)
+
+    
+ 

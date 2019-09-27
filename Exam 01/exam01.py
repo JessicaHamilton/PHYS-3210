efa5edf179f7a48bc262d9defeed9c40c8cf6a20
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-09-27 09:52:57 -0400

    updates to exam01 and ex12

--- Exam 01/exam01.py
+++ Exam 01/exam01.py
@@ -14,49 +14,56 @@ def old_cherry(N):
     #initialize values for the tree and the basket and set range for turns
     Cherry_tree = 10
     Basket = 0
+    turns_played = 0
     #set loop for number of spins
-    for turn in range(N):
+    for turn in range(1,N):
         #setup if statement to randomly generate a value between 0 and 1 for the weight of the spin
         weight = rand.random()
-        if weight >= 0.75:
-            spin = rand.randint(4,6)
-        else:
+        if weight <= 0.75:
             spin = rand.randint(0,3)
-        if spin == 0:
-            Cherry_tree = Cherry_tree - 1
-            Basket = Basket + 1
-            if Basket == 10:
-                break
-        elif spin == 1:
-            Cherry_tree = Cherry_tree - 2
-            Basket = Basket + 2
-            if Basket == 10:
-                break
-        elif spin == 2:
-            Cherry_tree = Cherry_tree - 3
-            Basket = Basket + 3
-            if Basket == 10:
-                break
-        elif spin == 3:
-            Cherry_tree = Cherry_tree -4
-            Basket = Basket + 4
-            if Basket == 10:
-                break
-        elif spin == 4 or spin == 5:
-            if Basket < 2 and Basket > 0:
-                Cherry_tree = Cherry_tree +1
-                Basket = Basket -1
-            elif Basket >= 2:
-                Cherry_tree = Cherry_tree +2
-                Basket = Basket - 2
-            else:
-                continue
-        elif spin == 6:
-            length = Basket
-            Cherry_tree = Cherry_tree + length
-            Basket = 0
+        else:
+            spin = rand.randint(4,6)
+            
+            #determine what happens with each spin    
+            if spin == 0:
+                Cherry_tree = Cherry_tree - 1
+                Basket = Basket + 1
+                if Basket == 10:
+                    turns_played = turn
+                    break
+            elif spin == 1:
+                Cherry_tree = Cherry_tree - 2
+                Basket = Basket + 2
+                if Basket == 10:
+                    turns_played = turn
+                    break
+            elif spin == 2:
+                Cherry_tree = Cherry_tree - 3
+                Basket = Basket + 3
+                if Basket == 10:
+                    turns_played = turn
+                    break
+            elif spin == 3:
+                Cherry_tree = Cherry_tree - 4
+                Basket = Basket + 4
+                if Basket == 10:
+                    turns_played = turn
+                    break
+            elif spin == 4 or spin == 5:
+                if Basket < 2 and Basket > 0:
+                    Cherry_tree = Cherry_tree +1
+                    Basket = Basket -1
+                elif Basket >= 2:
+                    Cherry_tree = Cherry_tree +2
+                    Basket = Basket - 2
+                else:
+                    continue
+            elif spin == 6:
+                length = Basket
+                Cherry_tree = Cherry_tree + length
+                Basket = 0
 
-        turns_played = turn
+        turns_played = turns_played
         return turns_played
 #Print(“The amount of turns it takes to win:”, turns)
 
@@ -65,6 +72,7 @@ def new_cherry(N):
     #initialize values for the tree and the basket and set range for turns
     Cherry_tree = 10
     Basket = 0
+    turns_played2 = 0
     for turns in range(N):
         #randomly generate integers between 0 and 7 to simulate the different options the spinner can land on
         spins = rand.randint(0,6,1)
@@ -73,21 +81,25 @@ def new_cherry(N):
             Cherry_tree = Cherry_tree - 1
             Basket = Basket + 1
             if Basket == 10:
+                turns_played2 = turns
                 break
         elif spins == 1:
             Cherry_tree = Cherry_tree - 2
             Basket = Basket + 2
             if Basket == 10:
+                turns_played2 = turns
                 break
         elif spins == 2:
             Cherry_tree = Cherry_tree - 3
             Basket = Basket + 3
             if Basket == 10:
+                turns_played2 = turns
                 break
         elif spins == 3:
             Cherry_tree = Cherry_tree - 4
             Basket = Basket + 4
             if Basket == 10:
+                turns_played2 = turns
                 break
         elif spins == 4 or spins == 5:
             if Basket < 2 and Basket >=0:
@@ -102,13 +114,35 @@ def new_cherry(N):
             lengths = Basket
             Cherry_tree = Cherry_tree + lengths
             Basket = 0
-    turn_played = turns
-    return turn_played
+    turn_played2 = turns_played2
+    return turn_played2
 
 
 
 test1 = old_cherry(50)
-print("Turns for old version:",(test1+1))
+#print("Turns for old version:",(test1+1))
 test2 = new_cherry(50)
-print("Turns for new version:",(test2+1))    
+#print("Turns for new version:",(test2+1))    
 
+
+old_array = []
+new_array = []
+for each_num in range(10):
+    test1 = old_cherry(1000)
+    count = test1+1
+    old_array.append(test1)
+    averageold = np.mean(old_array)
+    
+    test2 = new_cherry(1000)
+    count2 = test2+1
+    new_array.append(test2)
+    averagenew = np.mean(new_array)
+    
+
+print("Turns for old version:",averageold)   
+print("Turns for new version:",averagenew)    
+    
+    
+    
+    
+    
--- Week 06/exercise12.py
+++ Week 06/exercise12.py
@@ -17,32 +17,42 @@ def meanvalue_Int(N, initial,final):
     #for loop for N-D
     sum_array = []
     mean_array = []
+    x_array = []
+    y_array = []
     for n in range(N):
         #Generate the sampling values for x  and calculate y values
         #for n in range(N):
         x = rand.uniform(initial,final)
+        x_array.append(x)
         sum_x = np.sum(x)
-        y = x**2
+        y = sum_x**2
+        y_array.append(y)
         
-        
-        
-        #calculate mean value and append to array
-        mean_value = (final - initial)
-        mean_array.append(mean_value)
+    #calculate mean value and append to array
+    mean_some = 1
+    y_mean = np.sum(y)
+    result = 1*y_mean
+    """
+    first = y_array[0]
+    last = y_array[-1]
+    mean_value = abs(last - first)
+    mean_array.append(mean_value)
         
     total_mean = np.prod(mean_array)
     total_sum = np.sum(sum_array)
     result = (total_mean*total_sum)/N
-    return result, x, y
+    """
+    return y_mean, result, x_array, #y_array
     
     
 
-a = meanvalue_Int(1000,0,10,10)
-b = meanvalue_Int(1000,0,10,10)
-c = meanvalue_Int(1000,0,10,10)
-d = meanvalue_Int(1000,0,10,10)
+a,aa,aaa = meanvalue_Int(1000,0,10)
+b,bb,bbb = meanvalue_Int(1000,0,10)
+c,cc,ccc = meanvalue_Int(1000,0,10)
+d,dd,ddd = meanvalue_Int(1000,0,10)
 
 print(a,b,c,d)
+#print(aaa)
 
     
  

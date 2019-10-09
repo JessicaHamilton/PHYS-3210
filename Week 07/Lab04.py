39e4f6b4cab4d1c1017ea694277bac5795a0fd3e
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-10-09 12:32:36 -0400

    Update Lab04.py

--- Week 07/Lab04.py
+++ Week 07/Lab04.py
@@ -16,15 +16,15 @@ L1 = 3
 L2 = 4
 L3 = 4
 L = 8
-sth1 = 1
-sth2 = 1
-sth3 = 1
-cth1 = 1
-cth2 = 1
-cth3 = 1
-T1 = 1
-T2 = 1
-T3 = 1 
+sth1 = 0.500
+sth2 = 0.2588
+sth3 = 0.7071
+cth1 = 0.8660
+cth2 = 0.9659
+cth3 = 0.7971
+T1 = 5
+T2 = 5
+T3 = 5 
 
 
 #define functions and calculate initial function values
@@ -39,7 +39,7 @@ y8 = T2*sth2 + T3*sth3 - W2
 y9 = T2*cth2 + T3*cth3
 
 f_vector = np.array([[y1],[y2],[y3],[y4],[y5],[y6],[y7],[y8],[y9]])
-
+print(f_vector)
 #find the partials for the functions per y
 yy1 = 0 + 0 + 0 + L1 + L2 + L3 + 0 + 0 + 0 
 yy2 = L1 + L2 + L3 + 0 + 0 + 0 + 0 + 0 + 0
@@ -52,12 +52,15 @@ yy8 = 0 + T2 + T3 + 0 + 0 + 0 + sth2 + sth3 + 0
 yy9 = 0 + 0 + 0 + 0 + T2 - T3 + 0 + cth2 - cth3
 
 jac = np.array([[yy1],[yy2],[yy3],[yy4],[yy5],[yy6],[yy7],[yy8],[yy9]])
+print(jac)
+inv_jac = 1 / jac
+print(inv_jac)
 
 #Start iteration for determining values
 for n in range(20):
     #calculate dx
-    inv_jac = 1 / jac
-    dx = -(inv_jac)*f_vector
+    dx = np.negative(inv_jac)*f_vector
+    break
     #update guesses with dx
     sth1 = sth1 + dx
     sth2 = sth2 + dx
@@ -80,9 +83,11 @@ for n in range(20):
     y9 = T2*cth2 + T3*cth3
     #redefine f_vector
     f_vector = np.array([[y1],[y2],[y3],[y4],[y5],[y6],[y7],[y8],[y9]])
+    print(f_vector)
     #set condition to exit for loop
-    if dx <= 10e-6:
-        break
+    for d in range(0, len(dx)):
+        if d <= 10e-6:
+            break
 print(T1,T2,T3,sth1,sth2,sth3,cth1,cth2,cth3)
 
 

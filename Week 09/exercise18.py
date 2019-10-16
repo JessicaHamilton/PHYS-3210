48f516af8288ff31f6118618014d593ee1de0216
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-10-16 10:23:28 -0400

    Create exercise18.py

---
+++ Week 09/exercise18.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 10:17:06 2019
+
+@author: hamil
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+from scipy import optimize
+
+
+
+
+data = np.loadtxt('pi_meson_decays.dat')
+print(data)

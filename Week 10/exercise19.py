491b59257fae7e8a8cd76e05860b076fda98a2f7
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-10-23 09:55:04 -0400

    Update exercise19.py

--- Week 10/exercise19.py
+++ Week 10/exercise19.py
@@ -10,13 +10,19 @@ import matplotlib.pyplot as plt
 
 
 
-
-def func(x_int, v_int, h, m, P):
+def func(x_int, v_int, h, m,P):
     x_array = []
     v_array = []
+    v = 0
     k = 1
-    time = np.arange(0,20,h)
+    time = np.arange(0,50,1)
     for t in time:
-        v = v_int + (h/m)*(-k*((x_int)**(p-1)))
+        v = v_int + (h/m)*(-k*((x_int)**(point-1)))
         v_array.append(v)
-
+    return v_array
+P = np.arange(2,12,2)
+time = np.arange(0,50,1)
+for point in P:
+    test = func(2,0,0.1,1,P)
+    plt.scatter(time, test)
+    

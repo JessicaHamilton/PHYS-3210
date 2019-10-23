630393f3c0ce72db4f756ba69624519d1a6ce35c
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-10-23 12:25:00 -0400

    Update exercise19.py

--- Week 10/exercise19.py
+++ Week 10/exercise19.py
@@ -11,18 +11,26 @@ import matplotlib.pyplot as plt
 
 
 def func(x_int, v_int, h, m,P):
+    k = 0.5
+    #create variables
+    xnew = x_int + v_int*h
+    anew = (-k/m)*x_int**(P-1)
+    vnew = v_int + anew*h
     x_array = []
     v_array = []
-    v = 0
-    k = 1
-    time = np.arange(0,50,1)
+    time = np.arange(0,50,0.1)
     for t in time:
-        v = v_int + (h/m)*(-k*((x_int)**(point-1)))
-        v_array.append(v)
-    return v_array
+        xnew = xnew + vnew*h
+        x_array.append(xnew)
+        vnew = vnew + anew*h
+        v_array.append(vnew)
+    return v_array, x_array, time
+
+trialv, trialx, trialt = func(1,1,0.1,1,4)
+plt.scatter(time,trialx)
+plt.show()
+#varying P
 P = np.arange(2,12,2)
-time = np.arange(0,50,1)
 for point in P:
-    test = func(2,0,0.1,1,P)
-    plt.scatter(time, test)
-    
+    testv, testx, time = func(2,0.5,0.1,1,point)
+    plt.scatter(time, testx, s=3)

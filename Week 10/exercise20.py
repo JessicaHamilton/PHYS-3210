cc8d38f3a26f5249d4677c896efb2ed2895832ab
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-10-28 21:19:41 -0400

    complete exercise 20 and writeup

--- Week 10/exercise20.py
+++ Week 10/exercise20.py
@@ -30,18 +30,18 @@ def rkfunc(x_int, v_int, h, P):
     return v_array, x_array, time
 
 
-testv, testx, testt = rkfunc(1,0,0.0001, 4)    
+testv, testx, testt = rkfunc(1,0.5,0.0001, 4)    
 plt.plot(testx, testt)
 plt.title('Runga Kutta Method First Plot')
 plt.show()
 
 #to vary P we can find:
 
-test1, test11, test111 = rkfunc(1, 0, 0.0001, 2)
-test2, test22, test222 = rkfunc(1, 0, 0.0001, 4)
-test3, test33, test333 = rkfunc(1, 0, 0.0001, 6)
-test4, test44, test444 = rkfunc(1, 0, 0.0001, 8)
-test5, test55, test555 = rkfunc(1, 0, 0.0001, 10)
+test1, test11, test111 = rkfunc(2, 0, 0.0001, 2)
+test2, test22, test222 = rkfunc(2, 0, 0.0001, 4)
+test3, test33, test333 = rkfunc(2, 0, 0.0001, 6)
+test4, test44, test444 = rkfunc(2, 0, 0.0001, 8)
+test5, test55, test555 = rkfunc(2, 0, 0.0001, 10)
 
 
 plt.plot(test11, test111)
@@ -54,14 +54,14 @@ plt.plot(test44, test444)
 plt.plot(test55, test555)
 plt.title('Runga Kutta Method over with P=6,8,10')
 plt.show()
-
+"""
 P = np.arange(2,12, 2)
 for p in P:
-    t, tt, ttt = rkfunc(1, 0, 0.0001, p)
+    t, tt, ttt = rkfunc(2, 0, 0.0001, p)
     plt.plot(tt, ttt)
 
 plt.show()
-
+"""
 
 #Now compare the two methods:
 
@@ -85,47 +85,47 @@ def func(x_int, v_int, h,P):
     return v_array, x_array, time
 
 
-testa, testaa, testaaa = rkfunc(1, 0, 0.0001, 2)
-testb, testbb, testbbb = rkfunc(1, 0, 0.0001, 4)
-testc, testcc, testccc = rkfunc(1, 0, 0.0001, 6)
-testd, testdd, testddd = rkfunc(1, 0, 0.0001, 8)
-teste, testee, testeee = rkfunc(1, 0, 0.0001, 10)
+testa, testaa, testaaa = rkfunc(2, 0, 0.0001, 2)
+testb, testbb, testbbb = rkfunc(2, 0, 0.0001, 4)
+testc, testcc, testccc = rkfunc(2, 0, 0.0001, 6)
+testd, testdd, testddd = rkfunc(2, 0, 0.0001, 8)
+teste, testee, testeee = rkfunc(2, 0, 0.0001, 10)
 
 plt.subplot(121)
 plt.plot(test11, test111)
 plt.plot(test22, test222)
-plt.plot(test33, test333)
-plt.title('Runga Kutta Method')
+#plt.plot(test33, test333)
+plt.title('Runga Kutta Method: Ts:0.0001')
 plt.subplot(122)
 plt.plot(testaa, testaaa)
-plt.plot(testbb, testbbb)
+#plt.plot(testbb, testbbb)
 plt.plot(testcc, testccc)
-plt.title('Eulers Method')
+plt.title('Eulers Method: Ts: 0.0001')
 
 plt.show()
 
 #Changing step sizes
-test1, test11, test111 = rkfunc(1, 0, 0.00025, 2)
-test2, test22, test222 = rkfunc(1, 0, 0.00025, 4)
-test3, test33, test333 = rkfunc(1, 0, 0.00025, 6)
-test4, test44, test444 = rkfunc(1, 0, 0.00025, 8)
-test5, test55, test555 = rkfunc(1, 0, 0.00025, 10)
+test1, test11, test111 = rkfunc(1, 0, 0.0005, 2)
+test2, test22, test222 = rkfunc(1, 0, 0.0005, 4)
+test3, test33, test333 = rkfunc(1, 0, 0.0005, 6)
+test4, test44, test444 = rkfunc(1, 0, 0.0005, 8)
+test5, test55, test555 = rkfunc(1, 0, 0.0005, 10)
 
-testa, testaa, testaaa = rkfunc(1, 0, 0.00025, 2)
-testb, testbb, testbbb = rkfunc(1, 0, 0.00025, 4)
-testc, testcc, testccc = rkfunc(1, 0, 0.00025, 6)
-testd, testdd, testddd = rkfunc(1, 0, 0.00025, 8)
-teste, testee, testeee = rkfunc(1, 0, 0.00025, 10)
+testa, testaa, testaaa = func(1, 0, 0.0005, 2)
+testb, testbb, testbbb = func(1, 0, 0.0005, 4)
+testc, testcc, testccc = func(1, 0, 0.0005, 6)
+testd, testdd, testddd = func(1, 0, 0.0005, 8)
+teste, testee, testeee = func(1, 0, 0.0005, 10)
 
 plt.subplot(121)
 plt.plot(test11, test111)
 plt.plot(test22, test222)
-plt.plot(test33, test333)
-plt.title('Runga Kutta Method')
+#plt.plot(test33, test333)
+plt.title('Runga Kutta Method: Ts:0.0005')
 plt.subplot(122)
 plt.plot(testaa, testaaa)
-plt.plot(testbb, testbbb)
+#plt.plot(testbb, testbbb)
 plt.plot(testcc, testccc)
-plt.title('Eulers Method')
+plt.title('Eulers Method: Ts:0.0005')
 
 

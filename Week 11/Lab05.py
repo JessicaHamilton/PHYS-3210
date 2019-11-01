a64606918ad64821d4667d8cb78793190e06ad6a
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-01 09:30:54 -0400

    Update Lab05.py

--- Week 11/Lab05.py
+++ Week 11/Lab05.py
@@ -121,3 +121,66 @@ plt.title('Velocity vs. time: Euler')
 plt.xlabel('Velocity')
 plt.ylabel('time')
 plt.show()
+
+
+
+
+
+#Now to look into adding driving forces
+
+def rk_drivefunc(x_int, v_int, h, P):
+    k = 10
+    m = 2
+    g = 9.8
+    mu_s = 0.1
+    mu_k = 0.1
+    omega = 1
+    f_o = 5
+    x_array = []
+    v_array = []
+    #create initial steps
+    xnew = x_int
+    vnew = v_int
+    time = np.arange(0,10, h)
+    for t in time:
+        #define forces and variables:
+        vmag1 = abs(vnew)
+        f_static1 = -mu_s*m*g
+        f_kin1 = -mu_k*m*g*(vnew/vmag1)
+        f_resto = -k*(xnew**(P-1))
+        f_drive = f_o*np.sin(omega*(h/2))
+        
+        #calculate half steps
+        v_half = vnew + (f_resto/m + f_drive/m)*(h/2)
+        x_half = xnew + vnew*(h/2)
+        
+        #redefine func and update x and v values for full steps
+        vmag2 = abs(v_half)
+        f_static2 = -mu_s*g*m
+        f_kin2 = -mu_k*g*m*(v_half/vmag2)
+        f_resto2 = -k*(x_half**(P-1))
+        f_drive = f_o*np.sin(omega*(h))
+        
+        vnew = vnew + (f_resto2/m + f_drive/m)*h
+        xnew = xnew + (v_half)*h
+        
+        #append to arrays
+        v_array.append(vnew)
+        x_array.append(xnew)
+        """
+        if vnew == 0:
+            if f_rest > f_static:
+                continue
+            else:
+                break
+        """
+    return v_array, x_array, time
+
+ww, www, wwww = rk_drivefunc(1,1e-16,0.0001,2)
+plt.plot(www, wwww)
+plt.title('Position versus Time: Driving Force')
+plt.show()
+"""
+plt.plot(ww, wwww)
+plt.title('Velocity versus Time: Driving Force')
+"""

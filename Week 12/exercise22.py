3a3ce27f4f45e5b7425f05248f37d56460ac855b
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-04 10:54:51 -0500

    Create exercise22.py

---
+++ Week 12/exercise22.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 10:39:16 2019
+
+@author: hamil
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#define functions and variables
+y_0 = 
+k = -(2*m*E)/h_bar**2
+y_1 = 0.95*np.e**(-k*x)
+y_2 = (2*m/h_bar**2)*v_x*y_0 + k*y_0
+
+

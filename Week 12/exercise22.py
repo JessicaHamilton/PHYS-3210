586af786f0e3631f8618c76f039ee4a1718df6eb
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-06 15:35:59 -0500

    Update exercise22.py

--- Week 12/exercise22.py
+++ Week 12/exercise22.py
@@ -8,10 +8,49 @@ Created on Mon Nov  4 10:39:16 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-#define functions and variables
-y_0 = 
-k = -(2*m*E)/h_bar**2
-y_1 = 0.95*np.e**(-k*x)
-y_2 = (2*m/h_bar**2)*v_x*y_0 + k*y_0
+
+
+
+#Outside of boundaries
+#from left
+
+k_sq = 2
+m=1
+x_match = 1
+a = 2 
+h=0.1
+y_array = []
+x_array = []
+V_o = -83.0 
+V = 0
+ynew = 0
+xnew = -20
+x_array = np.arange(-20,20,h)
+for value in x_array:
+    if abs(value) <= a:
+        ynew = ynew + ((-0.0483*V_o*ynew + k_sq*ynew)/m)*h
+    elif abs(value) > a:
+        ynew = -k_sq*np.exp((-k_sq)*abs(value))
+        
+    
+    #update values and append to an array
+    y_array.append(ynew)
+
+plt.plot(x_array, y_array)
+plt.show() 
+
+
+
+
+
+
+#Just looking out out function
+x = np.arange(-20,20,0.1)
+yy = []
+for each in x:
+    y = -k_sq*np.exp((-k_sq)*abs(each))
+    yy.append(y)
+plt.plot(x,yy)
+
 
 

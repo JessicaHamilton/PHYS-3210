6e94e9ff3e3211a3148869f5a78e38a8d3ed0ecf
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-13 10:54:00 -0500

    Update exercise23.py

--- Week 13/exercise23.py
+++ Week 13/exercise23.py
@@ -11,13 +11,12 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 #define variables
-m = 0.5
-delta_b = 0.05
-b = 0.25 # between -1 and 1
-next_x = 1
-next_y = 1
+m = 0.1
+next_x = 0.12
+next_y = 0.12
+b = np.sqrt(next_x**2+ next_y**2)
 next_vx = 0
-next_vy = 0.5
+next_vy = 0.05
 h = 0.0001
 
 
@@ -27,35 +26,45 @@ x_arr = []
 y_arr = []
 vx_arr = []
 vy_arr = []
-
+pot_arr = []
 
 #begin loop
-time = np.arange(0,50,h)
+time = np.arange(0,100,h)
 for t in time:
-    #define functions
-    F_x = 2*next_x*next_y**2*(1-next_x**2)*np.exp(-1*(next_x**2+next_y**2))
-    F_y = 2*next_y*next_x**2*(1-next_y**2)*np.exp(-1*(next_x**2+next_y**2))
-    
-    #update values
-    next_vx = next_vx + F_x*h
-    next_vy = next_vy + F_y*h
-    next_x = next_x + next_vx*h
-    next_y = next_y + next_vy*h
-    
-    #append new values
-    x_arr.append(next_x)
-    y_arr.append(next_y)
-    vx_arr.append(next_vx)
-    vy_arr.append(next_vy)
-    #theta = np.arctan(next_vy/next_vx)
+    if b < 0.95:
+        #define functions
+        F_x = -2*next_x*next_y**2*(1-next_x**2)*np.exp(-1*(next_x**2+next_y**2))
+        F_y = -2*next_y*next_x**2*(1-next_y**2)*np.exp(-1*(next_x**2+next_y**2))
+        
+        #update values
+        next_vx = next_vx + (F_x/m)*h
+        next_vy = next_vy + (F_y/m)*h
+        next_x = next_x + next_vx*h
+        next_y = next_y + next_vy*h
+        
+        #Potential func
+        poten = (next_x**2*next_y**2)*np.exp(-1*(next_x**2+next_y**2))
+        pot_arr.append(poten)
+        #append new values
+        x_arr.append(next_x)
+        y_arr.append(next_y)
+        vx_arr.append(next_vx)
+        vy_arr.append(next_vy)
+        #theta = np.arctan(next_vy/next_vx)
+    else:
+        break
+
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(x_arr,y_arr, time)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Time')
 plt.show()
 
-
+"""
 plt.plot(time, x_arr)
 plt.title('Position-X vs Time')
 plt.show()
@@ -63,7 +72,20 @@ plt.plot(time, y_arr)
 plt.title('Position-Y vs Time')
 plt.show()
 plt.plot(time, vx_arr)
+plt.title('Velocity-X vs. Time')
 plt.show()
-plt.plot(time, vx_arr)
+plt.plot(time, vy_arr)
+plt.title('Velocity-Y vs. Time')
 plt.show()
+"""
+plt.plot(x_arr,y_arr)
+plt.title('Position-Y vs Position-X')
+plt.show()
+plt.plot(vx_arr, x_arr)
+plt.title('Velocity-X vs. Position-X')
+plt.show()
+plt.plot(vy_arr, y_arr)
+plt.title('Velocity-Y vs. Position-Y')
+plt.show()
+
 

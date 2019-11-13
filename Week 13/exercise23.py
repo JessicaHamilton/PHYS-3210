7add8e7d633a418696ab16d477a5fdd467c7bbd5
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-13 09:11:05 -0500

    Update exercise23.py

--- Week 13/exercise23.py
+++ Week 13/exercise23.py
@@ -10,10 +10,13 @@ import matplotlib.pyplot as plt
 
 #define variables
 m = 0.5
-v_y = 0.5
-v_x = 0
 delta_b = 0.05
 b = 0.25 # between -1 and 1
+next_x = 0
+next_y = 0
+next_vx = 0
+next_vy = 0.5
+
 
 #define functions
 y_0 = x
@@ -27,17 +30,19 @@ y_arr = []
 vx_arr = []
 vy_arr = []
 
-#begin loop
 
-next_x = next_x + (y_0*np.cos(theta))+((y_2*np.cos(theta))/m*(delta_b))
-x_arr.append(next_x)
-next_y = next_y + (y_1*np.sin(theta))+((y_3*np.sin(theta))/m*(delta_b))
-y_arr.append(next_y)
-next_vx = next_vx + next_vx*np.cos(theta)
-vx_arr.append(next_vx_arr)
-next_vy = next_vy + next_vy*np.sin(theta) + y_3/m*(delta_b)
-vy_arr.append(next_vy_arr)
-theta = np.arctan(next_vy/next_vx)
+#begin loop
+time = np.arange(0,100, 0.001)
+for t in time:
+    next_x = next_x + (next_x*np.cos(theta))+((y_2*np.cos(theta))/m*(delta_b))
+    x_arr.append(next_x)
+    next_y = next_y + (y_1*np.sin(theta))+((y_3*np.sin(theta))/m*(delta_b))
+    y_arr.append(next_y)
+    next_vx = next_vx + next_vx*np.cos(theta)
+    vx_arr.append(next_vx_arr)
+    next_vy = next_vy + next_vy*np.sin(theta) + y_3/m*(delta_b)
+    vy_arr.append(next_vy_arr)
+    theta = np.arctan(next_vy/next_vx)
 
 plt.plot(x_arr, y_arr)
 

07c0284d970b07c7695e96b85c49291cbba42586
JessicaHamilton <42654893+JessicaHamilton@users.noreply.github.com>
2019-11-22 10:39:17 -0500

    complete exercise 22

--- Week 12/exercise22.py
+++ Week 12/exercise22.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
     
 #define variables
-E = -10 # Must be negative
+E = -16.059999999995185# -15.9988 # Must be negative
 g = 0.0483 # value provided in book
 kappa_2 = -g*E # overall positive
 kappa = np.sqrt(kappa_2) #overall positive
@@ -38,7 +38,7 @@ def d_psi(x): #deriv of potential function exp is overall neg and kappa is neg
 def d_2psi(x): #not used
     return kappa**2*np.exp(-kappa*abs(x))
 def d_2psi_in(x,V): #sec deriv of potential function where V is defined by sign (x) and kappa**2 is overall neg * psi function 
-    return (V*g + kappa_2)*psi(x)
+    return (V*g + kappa_2)
 
 
 #create arrays and initial variables using boundary conditions 
@@ -57,11 +57,11 @@ x_prior = x_array1[0]
 for value in x_array1:
     if abs(value) > a:
         V = 0 
-        dp = dp + d_2psi_in(value,V)*dx
+        dp = dp + d_2psi_in(x_prior,V)*p*dx
         p = p + dp*dx
     elif abs(value) <= a:
         V = V_o
-        dp = dp + d_2psi_in(x_prior,V)*dx
+        dp = dp + d_2psi_in(x_prior,V)*p*dx
         p = p + dp*dx
     x_prior = x_prior + dx
     dp_array1.append(dp)
@@ -77,11 +77,11 @@ x_prior = x_array2[0]
 for value in x_array2:
     if abs(value) > a:
         V = 0
-        dp = dp + d_2psi_in(value,V)*dx
+        dp = dp + d_2psi_in(x_prior,V)*p*dx
         p = p + dp*dx
     elif abs(value) <= a:
         V = V_o
-        dp = dp + d_2psi_in(x_prior,V)*dx
+        dp = dp + d_2psi_in(x_prior,V)*p*dx
         p = p + dp*dx
         
     x_prior = value
@@ -95,9 +95,6 @@ dE_arr2 = np.array(dE_arr2)
 dE_top = dE_arr1[-1]-dE_arr2[-1]
 dE_bot = dE_arr1[-1]+dE_arr2[-1]
 dE_total = dE_top/dE_bot
-print(dE_total)
-nextE = dE_total/10
-print(nextE)
 
 
 plt.plot(x_array1, p_array1)
@@ -105,104 +102,96 @@ plt.plot(x_array2, p_array2)
 plt.title('Potential vs. Position')
 plt.show()
 
-plt.plot(x_array1, dp_array1)
-plt.plot(x_array2, dp_array2)
-plt.title('D-potential versus Position')
-plt.show()
-
-
-
-
 
 
 
 
 def energy(E):
     #define variables
-    g = 0.0483
-    kappa_2 = g*abs(E)
-    kappa = np.sqrt(kappa_2)
-    a = 2
-    x_match = 1
-    dx = 0.001
-    V_o = -83.0 
+    g = 0.0483 # value provided in book
+    kappa_2 = -g*E # overall positive
+    kappa = np.sqrt(kappa_2) #overall positive
+    a = 2. #Boundry of well
+    x_match = 1. #Matching point
+    dx = 0.001 #step- size
+    V_o = -83.0 # value provided in book
     
     #initialize arrays
     dp_array1 = []
     p_array1 = []
     dp_array2 = []
     p_array2 = []
-    dE_arr1 = []
-    dE_arr2 = []
+    dE_arrL = []
+    dE_arrR = []
     
     #define functions to use
-    def psi(x):
-        return np.exp(-kappa*abs(x))
-    def d_psi(x):
+    def psi(x): # potential function where exp is overall neg
+        return np.exp(-(np.sign(x))*kappa*x)
+    def d_psi(x): #deriv of potential function exp is overall neg and kappa is neg 
         return -(np.sign(x))*kappa*np.exp(-kappa*abs(x))
-    def d_2psi(x):
+    def d_2psi(x): #not used
         return kappa**2*np.exp(-kappa*abs(x))
+    def d_2psi_in(x,V): #sec deriv of potential function where V is defined by sign (x) and kappa**2 is overall neg * psi function 
+        return (V*g + kappa_2)
     
-    def d_2psi_in(x,V):
-        return (V*g + kappa_2)*psi(x)
     
-    
-    #create arrays and variables
+    #create arrays and initial variables using boundary conditions 
     x_array1 = np.arange(-5,x_match, dx)
     x_array2 = np.arange(5, x_match, -dx)
-    p1 = np.exp(-kappa*x_array1[0])
-    p2 = np.exp(-kappa*x_array2[-1])
-    dp1 = -kappa*np.exp(-kappa*x_array1[0])
-    dp2 = -kappa*np.exp(-kappa*x_array2[-1])
+    p1 = psi(x_array1[0])
+    dp1 = d_psi(x_array1[0])
+    
     
     #Interate over functions
+    #set initial variables for LEFT direction/ x_array1
     dp = dp1
     p = p1
+    x_prior = x_array1[0]
+    #loop over array 1, should go from left to right until reaches boundary and then goes to elif statement
     for value in x_array1:
         if abs(value) > a:
             V = 0 
-            dp = dp + (d_2psi_in(value,V))*dx
+            dp = dp + d_2psi_in(x_prior,V)*p*dx
             p = p + dp*dx
-        else:
+        elif abs(value) <= a:
             V = V_o
-            dp = dp + (d_2psi_in(value,V))*dx
+            dp = dp + d_2psi_in(x_prior,V)*p*dx
             p = p + dp*dx
-        
+        x_prior = x_prior + dx
         dp_array1.append(dp)
         p_array1.append(p)
-        dE = dp/p 
-        dE_arr1.append(dE)
-        
-    dp = dp2
-    p = p2    
+        deltaE = dp/p 
+        dE_arrL.append(deltaE)
+    
+    #set initial variables for RIGHT direction / x_array2    
+    dp = dp1
+    p = p1
+    x_prior = x_array2[0]
+    #loop over array 2, should go right to left until it reaches boundary and switches to elif statement    
     for value in x_array2:
         if abs(value) > a:
             V = 0
-            dp = dp + d_2psi_in(value,V)*dx
+            dp = dp + d_2psi_in(x_prior,V)*p*dx
             p = p + dp*dx
-        else:
+        elif abs(value) <= a:
             V = V_o
-            dp = dp + d_2psi_in(value,V)*dx
+            dp = dp + d_2psi_in(x_prior,V)*p*dx
             p = p + dp*dx
-        
+            
+        x_prior = value
         dp_array2.append(dp)
         p_array2.append(p)    
         deltaE = dp/p 
-        dE_arr2.append(deltaE)
+        dE_arrR.append(deltaE)
     
-    dE_arr1 = np.array(dE_arr1)
-    dE_arr2 = np.array(dE_arr2)
-    dE_top = dE_arr1[-1]-dE_arr2[-1]
-    dE_bot = dE_arr1[-1]+dE_arr2[-1]
-    dE_total = dE_top/dE_bot
+    dE_arrL = np.array(dE_arrL)
+    dE_arrR = np.array(dE_arrR)
+    dE_total = (dE_arrL[-1]-dE_arrR[-1])/(dE_arrL[-1]+dE_arrR[-1])
     return dE_total
-"""  
-E = -10    
-for n in range(50):
-    new = energy(E)
-    if abs(new) <= 1.0e-4:
+
+e_array = np.arange(-20,-10, dx)
+for value in e_array:
+    total = energy(value)
+    if total < dx:
         break
-    else:
-        E = E-(new*5)
-print(E, new)
-"""   
+print('Estimated value for initial energy:', value)
